ff3d3ec19ab2a7ecbbbb5d23de38f28e12e5e4ae
David Hampton <mythtv@love2code.net>
2021-06-25 23:20:49 -0400

    Replace OSDaB Project unzip code with libzip in mythcoreutil.cpp.
    The OSDaB project unzip code is from circa 2008, and can't process
    files zipped on current versions of linux.  Replace it with libzip,
    which seems to be well supported on all platforms.

--- mythtv/libs/libmythbase/mythcoreutil.cpp
+++ mythtv/libs/libmythbase/mythcoreutil.cpp
@@ -8,6 +8,11 @@
 // System specific C headers
 #include "compat.h"
 
+#include "zlib.h"
+#undef Z_NULL
+#define Z_NULL nullptr
+#include "zip.h"
+
 #ifdef linux
 #include <sys/vfs.h>
 #include <sys/sysinfo.h>
@@ -25,15 +30,26 @@
 // Qt headers
 #include <QByteArray>
 #include <QStringList>
+#include <QDir>
 #include <QFile>
 
 // libmythbase headers
 #include "mythcorecontext.h"
+#include "mythdate.h"
 #include "mythlogging.h"
-#include "unzip.h"
 
 #include "version.h"
 #include "mythversion.h"
+
+#define ZIP_ATTR_FILE_TYPE_MASK      0xFE000000
+#define   ZIP_ATTR_FILE_TYPE_SYMLINK 0xA0000000
+#define   ZIP_ATTR_FILE_TYPE_NORMAL  0x80000000
+#define ZIP_ATTR_USER_PERM_MASK      0x01C00000
+#define ZIP_ATTR_GROUP_PERM_MASK     0x03800000
+#define ZIP_ATTR_OTHER_PERM_MASK     0x00700000
+#define ZIP_ATTR_USER_PERM_SHIFT     22
+#define ZIP_ATTR_GROUP_PERM_SHIFT    19
+#define ZIP_ATTR_OTHER_PERM_SHIFT    16
 
 /** \fn getDiskSpace(const QString&,long long&,long long&)
  *  \brief Returns free space on disk containing file in KiB,
@@ -71,32 +87,249 @@ int64_t getDiskSpace(const QString &file_on_disk,
     return freespace;
 }
 
-bool extractZIP(const QString &zipFile, const QString &outDir)
-{
-    UnZip uz;
-    UnZip::ErrorCode ec = uz.openArchive(zipFile);
+// NOLINTNEXTLINE(readability-uppercase-literal-suffix)
+static constexpr uint64_t STATS_REQUIRED {
+    ZIP_STAT_NAME | ZIP_STAT_INDEX | ZIP_STAT_SIZE |
+    ZIP_STAT_MTIME | ZIP_STAT_ENCRYPTION_METHOD};
 
-    if (ec != UnZip::Ok)
+bool zipCreateDirectory(const QFileInfo & fi);
+bool zipCreateDirectory(const QFileInfo & fi)
+{
+    QDir dir = fi.absoluteDir();
+    if (dir.exists())
+        return true;
+    if (dir.mkpath(dir.absolutePath()))
+        return true;
+    LOG(VB_GENERAL, LOG_ERR,
+        QString("extractZIP(): Failed to create directory %1")
+        .arg(dir.absolutePath()));
+    return false;
+}
+
+bool zipValidateFilename(const QFileInfo& fi, const QString& outDirAbsPath);
+bool zipValidateFilename(const QFileInfo& fi, const QString& outDirAbsPath)
+{
+    if (fi.absoluteFilePath().startsWith(outDirAbsPath))
+        return true;
+    LOG(VB_GENERAL, LOG_ERR,
+        QString("extractZIP(): Attempt to write outside destination directory. File: %1")
+        .arg(QString(fi.fileName())));
+    return false;
+}
+
+void zipSetFileAttributes(QFile& outfile, const zip_stat_t& stats, zip_uint32_t attributes);
+void zipSetFileAttributes(QFile& outfile, const zip_stat_t& stats, zip_uint32_t attributes)
+{
+#if QT_VERSION >= QT_VERSION_CHECK(5,10,0)
+    // Set times
+    auto dateTime = MythDate::fromSecsSinceEpoch(stats.mtime);
+
+    outfile.setFileTime(dateTime, QFileDevice::FileAccessTime);
+    outfile.setFileTime(dateTime, QFileDevice::FileBirthTime);
+    outfile.setFileTime(dateTime, QFileDevice::FileMetadataChangeTime);
+    outfile.setFileTime(dateTime, QFileDevice::FileModificationTime);
+#endif
+
+    if (attributes == 0)
+        return;
+    int32_t user  = (attributes & ZIP_ATTR_USER_PERM_MASK)  >> ZIP_ATTR_USER_PERM_SHIFT;
+    int32_t group = (attributes & ZIP_ATTR_GROUP_PERM_MASK) >> ZIP_ATTR_GROUP_PERM_SHIFT;
+    int32_t other = (attributes & ZIP_ATTR_OTHER_PERM_MASK) >> ZIP_ATTR_OTHER_PERM_SHIFT;
+    int32_t qt_perms = (user << 12) | (user < 8) | (group < 4) | other;
+    outfile.setPermissions(static_cast<QFileDevice::Permission>(qt_perms));
+}
+
+bool zipCreateSymlink(zip_t* zip, zip_stat_t& stats, const QFileInfo& fi, const QString& outDirAbsPath);
+bool zipCreateSymlink(zip_t* zip, zip_stat_t& stats, const QFileInfo& fi, const QString& outDirAbsPath)
+{
+    zip_file_t *infile = zip_fopen_index(zip, stats.index, 0);
+    if (infile == nullptr)
+    {
+        LOG(VB_GENERAL, LOG_ERR,
+            QString("extractZIP(): Unable to open file %1 in %2")
+            .arg(stats.index).arg("abcdefg"));
+        return false;
+    }
+
+    int64_t  readLen      {0};
+    static constexpr int BLOCK_SIZE { 4096 };
+    QByteArray data; data.resize(BLOCK_SIZE);
+    readLen = zip_fread(infile, data.data(), BLOCK_SIZE);
+    if (readLen < 1)
+    {
+        LOG(VB_GENERAL, LOG_ERR,
+            QString("extractZIP(): Symlink name for file %1 is too short")
+            .arg(stats.index));
+        return false;
+    }
+    data.resize(readLen);
+
+    QFileInfo target = fi.absolutePath() + "/" + data;
+    if (!zipValidateFilename(target, outDirAbsPath))
+        return false;
+    if (!QFile::link(target.absoluteFilePath(), fi.absoluteFilePath()))
+    {
+        LOG(VB_GENERAL, LOG_ERR,
+            QString("extractZIP(): Failed to create link from %1 pointing to %2")
+            .arg(fi.absoluteFilePath())
+            .arg(target.absoluteFilePath()));
+        return false;
+    }
+    return true;
+}
+
+bool zipWriteOneFile(zip_t* zip, zip_stat_t& stats, const QFileInfo& fi, zip_uint32_t attributes);
+bool zipWriteOneFile(zip_t* zip, zip_stat_t& stats, const QFileInfo& fi, zip_uint32_t attributes)
+{
+    zip_file_t *infile = zip_fopen_index(zip, stats.index, 0);
+    if (infile == nullptr)
+    {
+        LOG(VB_GENERAL, LOG_ERR,
+            QString("extractZIP(): Unable to open file %1 in %2")
+            .arg(stats.index).arg("abcdefg"));
+        return false;
+    }
+
+    auto outfile = QFile(fi.absoluteFilePath());
+    if (!outfile.open(QIODevice::Truncate|QIODevice::WriteOnly))
+    {
+        LOG(VB_GENERAL, LOG_ERR,
+            QString("extractZIP(): Failed to open output file %1")
+            .arg(fi.absoluteFilePath()));
+        return false;
+    }
+
+    int64_t  readLen      {0};
+    uint64_t bytesRead    {0};
+    uint64_t bytesWritten {0};
+    static constexpr int BLOCK_SIZE { 4096 };
+    QByteArray data; data.resize(BLOCK_SIZE);
+    while ((readLen = zip_fread(infile, data.data(), BLOCK_SIZE)) > 0)
+    {
+        bytesRead += readLen;
+        int64_t writeLen = outfile.write(data.data(), readLen);
+        if (writeLen < 0)
+        {
+            LOG(VB_GENERAL, LOG_ERR,
+                QString("extractZIP(): Failed to write %1/%2 bytes to output file %3")
+                .arg(writeLen).arg(readLen).arg(fi.absoluteFilePath()));
+            return false;
+        }
+        bytesWritten += writeLen;
+    }
+
+    if ((stats.size != bytesRead) ||
+        (stats.size != bytesWritten))
+    {
+        LOG(VB_GENERAL, LOG_ERR,
+            QString("extractZIP(): Failed to copy file %1. Read %2 and wrote %3 of %4.")
+            .arg(fi.fileName()).arg(bytesRead).arg(bytesWritten)
+            .arg(stats.size));
+        return false;
+    }
+
+    outfile.flush();
+    zipSetFileAttributes(outfile, stats, attributes);
+    outfile.close();
+    if (zip_fclose(infile) == -1)
+    {
+        LOG(VB_GENERAL, LOG_ERR,
+            QString("extractZIP(): Failed to close current file %1")
+            .arg(fi.fileName()));
+        return false;
+    }
+
+    return true;
+}
+
+bool extractZIP(const QString &zipFileName, const QString &outDirName)
+{
+    auto outDir = QDir(outDirName);
+    if (!outDir.exists())
+    {
+        LOG(VB_GENERAL, LOG_ERR,
+            QString("extractZIP(): Target directory %1 doesn't exist")
+            .arg(outDirName));
+        return false;
+    }
+    auto outDirAbsPath = outDir.absolutePath();
+
+    int err { ZIP_ER_OK };
+    zip_t *zip = zip_open(qPrintable(zipFileName), 0, &err);
+    if (zip == nullptr)
     {
         LOG(VB_GENERAL, LOG_ERR,
                 QString("extractZIP(): Unable to open ZIP file %1")
-                        .arg(zipFile));
+                        .arg(zipFileName));
         return false;
     }
 
-    ec = uz.extractAll(outDir);
-
-    if (ec != UnZip::Ok)
+    zip_int64_t numEntries = zip_get_num_entries(zip, 0);
+    if (numEntries == -1)
     {
         LOG(VB_GENERAL, LOG_ERR,
-                QString("extractZIP(): Error extracting ZIP file %1")
-                        .arg(zipFile));
+                QString("extractZIP(): Zip archive %1 is empty")
+                        .arg(zipFileName));
         return false;
     }
 
-    uz.closeArchive();
+    bool ok { true };
+    for (int index = 0; ok && (index < numEntries); index++)
+    {
+        zip_stat_t stats;
+        zip_stat_init(&stats);
+        if (-1 == zip_stat_index(zip, index, 0, &stats))
+        {
+            LOG(VB_GENERAL, LOG_ERR,
+                QString("extractZIP(): Unable to get info for file %1 in %2")
+                .arg(index).arg(zipFileName));
+            return false;
+        }
+        if ((stats.valid & STATS_REQUIRED) != STATS_REQUIRED)
+        {
+            LOG(VB_GENERAL, LOG_ERR,
+                QString("extractZIP(): Invalid status information for file %1 in %2")
+                .arg(index).arg(zipFileName));
+            return false;
+        }
+        if (stats.encryption_method > 0)
+        {
+            LOG(VB_GENERAL, LOG_WARNING,
+                QString("extractZIP(): Skipping encryped file %1 in %2")
+                .arg(index).arg(zipFileName));
+            continue;
+        }
 
-    return true;
+        zip_uint8_t  opsys      {ZIP_OPSYS_UNIX};
+        zip_uint32_t attributes {0};
+        zip_file_get_external_attributes(zip, index, ZIP_FL_UNCHANGED, &opsys, &attributes);
+
+        auto fi = QFileInfo(outDirName + '/' + stats.name);
+        ok = zipValidateFilename(fi, outDirAbsPath);
+        if (ok)
+            ok = zipCreateDirectory(fi);
+        if (ok && (stats.size > 0))
+        {
+            switch (attributes & ZIP_ATTR_FILE_TYPE_MASK)
+            {
+              case ZIP_ATTR_FILE_TYPE_SYMLINK:
+                  ok = zipCreateSymlink(zip, stats, fi, outDirAbsPath);
+                break;
+              default:
+                  ok = zipWriteOneFile(zip, stats, fi, attributes);
+                break;
+            }
+        }
+    }
+
+    if (zip_close(zip) == -1)
+    {
+        LOG(VB_GENERAL, LOG_ERR,
+            QString("extractZIP(): Error closing ZIP file %1")
+            .arg(zipFileName));
+        return false;
+    }
+    return ok;
 }
 
 bool gzipFile(const QString &inFilename, const QString &gzipFilename)
