41df5c798c274055e5883773578ddb18c02d6354
David Hampton <mythtv@love2code.net>
2025-03-20 23:07:15 +0000

    cmake: Add tweaks for building for windows on a debian host.

--- cmake/platform/WindowsCrossSearchPaths.cmake
+++ cmake/platform/WindowsCrossSearchPaths.cmake
@@ -6,11 +6,23 @@
 
 # Look for precompiled libraries. Debian doesn't have many of these, but Fedora
 # has a bunch.
-set(MINGW_SYSROOT "/usr/${TOOLCHAIN_PREFIX}/sys-root/mingw")
-list(PREPEND PKG_CONFIG_PATH "${MINGW_SYSROOT}/lib/pkgconfig")
-list(APPEND CMAKE_FIND_ROOT_PATH "${MINGW_SYSROOT}/lib/pkgconfig")
-if(EXISTS ${MINGW_SYSROOT}/lib/libz.dll.a)
-  set(ZLIB_ROOT ${MINGW_SYSROOT})
-else()
-  set(ZLIB_ROOT ${LIBS_INSTALL_PREFIX})
+if(HOST_LSB_RELEASE_ID MATCHES "fedora")
+  set(MINGW_SYSROOT "/usr/${TOOLCHAIN_PREFIX}/sys-root/mingw")
+  list(PREPEND PKG_CONFIG_PATH "${MINGW_SYSROOT}/lib/pkgconfig")
+  list(APPEND CMAKE_FIND_ROOT_PATH "${MINGW_SYSROOT}/lib/pkgconfig")
+  if(EXISTS ${MINGW_SYSROOT}/lib/libz.dll.a)
+    set(ZLIB_ROOT ${MINGW_SYSROOT})
+  else()
+    set(ZLIB_ROOT ${LIBS_INSTALL_PREFIX})
+  endif()
+elseif(HOST_LSB_RELEASE_ID MATCHES "debian" OR HOST_LSB_RELEASE_ID MATCHES
+                                               "ubuntu")
+
+  # Choices are 14-win32 and 14-posix. Using 14-win32, all the
+  # libraries are built but the compilation fails in MythTV in
+  # mediamonitor-windows.cpp because of missing GTHREAD COND support.
+  # Using 14-posix to provide GTHREAD COND support, the mesa library
+  # can't be compiled because of missing pthread symbols.
+  set(MINGW_EXTRA_LIBRARY_PATH "-L/usr/lib/gcc/${TOOLCHAIN_PREFIX}/14-posix")
+  #set(MINGW_EXTRA_LIBRARY_PATH "-L/usr/lib/gcc/${TOOLCHAIN_PREFIX}/14-win32")
 endif()
