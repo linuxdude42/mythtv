8037c45e64f2149326e78f0d495f8d5ffc7781b6
David Hampton <mythtv@love2code.net>
2025-04-01 01:00:26 -0400

    tidy: Add braces around if-else clauses for consistency.
    https://clang.llvm.org/extra/clang-tidy/checks/readability/braces-around-statements.html

--- mythtv/libs/libmythui/mythuiwebbrowser.cpp
+++ mythtv/libs/libmythui/mythuiwebbrowser.cpp
@@ -205,13 +205,21 @@ bool MythWebEngineView::handleKeyPress(QKeyEvent *event)
             }
         }
         else if (action == "ZOOMIN")
+        {
             m_parentBrowser->ZoomIn();
+        }
         else if (action == "ZOOMOUT")
+        {
             m_parentBrowser->ZoomOut();
+        }
         else if (action == "RELOAD")
+        {
             m_parentBrowser->Reload(true);
+        }
         else if (action == "FULLRELOAD")
+        {
             m_parentBrowser->Reload(false);
+        }
         else if (action == "MOUSEUP" || action == "MOUSEDOWN" ||
                 action == "MOUSELEFT" || action == "MOUSERIGHT" ||
                 action == "MOUSELEFTBUTTON")
@@ -227,7 +235,9 @@ bool MythWebEngineView::handleKeyPress(QKeyEvent *event)
             m_parentBrowser->Forward();
         }
         else
+        {
             handled = false;
+        }
     }
 
     return handled;
