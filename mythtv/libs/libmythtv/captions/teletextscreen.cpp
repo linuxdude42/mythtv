1e9a7c38cd524025ba545923aaa5902b1a200f84
David Hampton <mythtv@love2code.net>
2025-05-08 19:00:28 -0400

    Convert the teletextscreen code to use C++ error handling.
    This replaces goto statements with try/throw/catch statements.

--- mythtv/libs/libmythtv/captions/teletextscreen.cpp
+++ mythtv/libs/libmythtv/captions/teletextscreen.cpp
@@ -383,6 +383,7 @@ void TeletextScreen::DrawLine(const tt_line_array& page, uint row, int lang)
         SetBackgroundColor(bgcolor);
 
         unsigned char ch = page[x] & 0x7F;
+        try
         {
             switch (ch)
             {
@@ -393,64 +394,59 @@ void TeletextScreen::DrawLine(const tt_line_array& page, uint row, int lang)
                     conceal = false;
                     // increment FLOF/FastText count if menu item detected
                     flof_link_count += (row == 25) ? 1 : 0;
-                    goto ctrl;
+                    throw "ctrl";
                 case 0x08: // flash
                     // XXX
-                    goto ctrl;
+                    throw "ctrl";
                 case 0x09: // steady
                     flash = false;
-                    goto ctrl;
+                    throw "ctrl";
                 case 0x0a: // end box
                     endbox = true;
-                    goto ctrl;
+                    throw "ctrl";
                 case 0x0b: // start box
                     if (x < kTeletextColumns - 1 && ((page[x + 1] & 0x7F) == 0x0b))
                         startbox = true;
-                    goto ctrl;
+                    throw "ctrl";
                 case 0x0c: // normal height
                     doubleheight = false;
-                    goto ctrl;
+                    throw "ctrl";
                 case 0x0d: // double height
                     doubleheight = (row < (kTeletextRows-1)) && (x < (kTeletextColumns - 1));
-                    goto ctrl;
+                    throw "ctrl";
     
                 case 0x10: case 0x11: case 0x12: case 0x13:
                 case 0x14: case 0x15: case 0x16: case 0x17: // graphics + foreground color
                     fgcolor = ch & 7;
                     mosaic = true;
                     conceal = false;
-                    goto ctrl;
+                    throw "ctrl";
                 case 0x18: // conceal display
                     conceal = true;
-                    goto ctrl;
+                    throw "ctrl";
                 case 0x19: // contiguous graphics
                     seperation = false;
-                    goto ctrl;
+                    throw "ctrl";
                 case 0x1a: // separate graphics
                     seperation = true;
-                    goto ctrl;
+                    throw "ctrl";
                 case 0x1c: // black background
                     bgcolor = kTTColorBlack;
-                    goto ctrl;
+                    throw "ctrl";
                 case 0x1d: // new background
                     bgcolor = fgcolor;
-                    goto ctrl;
+                    throw "ctrl";
                 case 0x1e: // hold graphics
                     hold = true;
-                    goto ctrl;
+                    throw "ctrl";
                 case 0x1f: // release graphics
                     hold = false;
-                    goto ctrl;
+                    throw "ctrl";
                 case 0x0e: // SO (reserved, double width)
                 case 0x0f: // SI (reserved, double size)
                 case 0x1b: // ESC (reserved)
                     ch = ' ';
                     break;
-            ctrl:
-                ch = ' ';
-                if (hold && mosaic)
-                    ch = last_ch;
-                break;
 
                 default:
                     if ((ch >= 0x80) && (ch <=0x9f)) // these aren't used
@@ -462,6 +458,10 @@ void TeletextScreen::DrawLine(const tt_line_array& page, uint row, int lang)
                     }
                     break;
             }
+        }
+        catch (char const* e)
+        {
+            ch = (hold && mosaic) ? last_ch : ' ';
         }
 
         // Hide FastText/FLOF menu characters if not available
