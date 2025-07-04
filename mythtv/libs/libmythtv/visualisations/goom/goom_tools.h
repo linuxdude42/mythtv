a5fe7beb3b80811f0d95587a36cbec033b245e84
David Hampton <mythtv@love2code.net>
2025-07-03 22:39:15 -0400

    tidy: Convert visualizations to std::vector. (part 2)

--- mythtv/libs/libmythtv/visualisations/goom/goom_tools.h
+++ mythtv/libs/libmythtv/visualisations/goom/goom_tools.h
@@ -4,6 +4,7 @@
 #include <cinttypes>
 #include <cstddef>
 #include <cstdlib>
+#include <vector>
 
 #if !defined( M_PI ) 
 static constexpr double M_PI    {  3.14159265358979323846  };
@@ -13,17 +14,18 @@ static constexpr float  M_PI_F  { static_cast<float>(M_PI) };
 static constexpr size_t NB_RAND { 0x10000 };
 
 /* in graphic.c */
-extern int *rand_tab;
+extern std::vector<int> rand_tab;
+// This is expected to wrap around.  It must support NB_RAND values.
 static unsigned short rand_pos;
 
 static inline void RAND_INIT(int i)
 {
     srand (i) ;
-    if (!rand_tab)
-        rand_tab = (int *) malloc (NB_RAND * sizeof(int)) ;
-    rand_pos = 1 ;
-    while (rand_pos != 0)
-        rand_tab[rand_pos++] = rand () ;
+    if (rand_tab.empty())
+        rand_tab.resize(NB_RAND) ;
+    for (size_t j = 0; j < NB_RAND; j++)
+        rand_tab[j] = rand() ;
+    rand_pos = 0;
 }
 
 static inline int RAND(void)
@@ -34,8 +36,7 @@ static inline int RAND(void)
 
 static inline void RAND_CLOSE()
 {
-    free(rand_tab);
-    rand_tab = nullptr;
+    rand_tab.clear();
 }
 
 //#define iRAND(i) ((guint32)((float)i * RAND()/RAND_MAX))
