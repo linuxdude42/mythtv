87198f3e0ab8095cf5f4df91845f8d3741e77946
David Hampton <mythtv@love2code.net>
2025-07-28 18:21:34 -0400

    Start converting lirc_client.cpp to std::string. lirc_code (tested)

--- mythtv/libs/libmythui/test/test_lirc/test_lirc.cpp
+++ mythtv/libs/libmythui/test/test_lirc/test_lirc.cpp
@@ -785,8 +785,8 @@ void TestLirc::test_code2char_internal(void)
     QVERIFY(m_config != nullptr);
 
     struct lirc_config_entry *entry = m_config->next;
-    char *remote = LIRC_ALL;
-    char *button = strdup("purple");
+    std::string remote = LIRC_ALL;
+    std::string button = strdup("purple");
     int level = lirc_iscode(entry, remote, button, 0);
     QCOMPARE(level, 0);
 
